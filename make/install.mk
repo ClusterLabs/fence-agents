4220491205dd5296581f09a51d84bbcdfaef718b
Fabio M. Di Nitto <fdinitto@redhat.com>
2009-01-07 12:17:49 +0100

    split tree into separate projects
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/install.mk
+++ make/install.mk
@@ -1,27 +1,10 @@
 install:
-ifdef LIBDIRT
-	install -d ${libdir}
-	install -m644 ${LIBDIRT} ${libdir}
-endif
-ifdef LIBSYMT
-	cp -a ${LIBSYMT} ${libdir}
-endif
-ifdef INCDIRT
-	install -d ${incdir}
-	for i in ${INCDIRT}; do \
-		install -m644 $(S)/$$i ${incdir}; \
-	done
-endif
 ifdef SBINDIRT
 	install -d ${sbindir}
 	install -m755 ${SBINDIRT} ${sbindir}
 endif
 ifdef SBINSYMT
 	cp -a ${SBINSYMT} ${sbindir}
-endif
-ifdef LCRSOT
-	install -d ${libexecdir}/lcrso
-	install -m644 ${LCRSOT} ${libexecdir}/lcrso
 endif
 ifdef INITDT
 	install -d ${initddir}
@@ -32,20 +15,6 @@ ifdef INITDT
 			install -m755 $(S)/$$i ${initddir}; \
 		fi; \
 	done
-endif
-ifdef UDEVT
-	install -d ${DESTDIR}/etc/udev/rules.d
-	for i in ${UDEVT}; do \
-		install -m644 $(S)/$$i ${DESTDIR}/etc/udev/rules.d; \
-	done
-endif
-ifdef KMODT
-	install -d ${module_dir}/${KDIRT}
-	install -m644 ${KMODT} ${module_dir}/${KDIRT}
-endif
-ifdef KHEADT
-	install -d ${incdir}/linux
-	install -m644 ${KHEADT} ${incdir}/linux
 endif
 ifdef MIBRESOURCE
 	install -d ${mibdir}
@@ -60,10 +29,6 @@ ifdef DOCS
 	for i in ${DOCS}; do \
 		install -m644 $(S)/$$i ${docdir}; \
 	done
-endif
-ifdef LOGRORATED
-	install -d ${logrotatedir}
-	install -m644 ${LOGRORATED} ${logrotatedir}
 endif
 ifdef NOTIFYD
 	install -d ${notifyddir}
