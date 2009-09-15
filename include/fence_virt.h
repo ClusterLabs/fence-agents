1ddb9136d9938a9af1a78bcbd1a1f27d76b6c3a3
Lon Hohberger <lhh@redhat.com>
2009-09-15 12:08:10 -0400

    More build cleanups
    Signed-off-by: Lon Hohberger <lhh@redhat.com>

--- include/fence_virt.h
+++ include/fence_virt.h
@@ -1,6 +1,10 @@
 #ifndef _FENCE_VIRT_H
 #define _FENCE_VIRT_H
 
-#define DEFAULT_CONFIG_FILE "/etc/fence_virt.conf"
+#ifdef SYSCONFDIR
+#define DEFAULT_CONFIG_FILE SYSCONFDIR "/fence_virt.conf"
+#else
+#define DEFAULT_CONFIG_FILE SYSCONFDIR "/etc/fence_virt.conf"
+#endif
 
 #endif
