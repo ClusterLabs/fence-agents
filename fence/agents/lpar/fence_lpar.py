55937766711d33f8941682cc76ac9fae1661c1ca
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-01-06 20:14:21 +0100

    fence lpar: fix quoting
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- fence/agents/lpar/fence_lpar.py
+++ fence/agents/lpar/fence_lpar.py
@@ -138,8 +138,9 @@ def main():
 
 	atexit.register(atexit_handler)
 
-	all_opt["login_timeout"]["default"] = 15
-	all_opt["secure"]["default"] = 1
+	all_opt["login_timeout"]["default"] = "15"
+	all_opt["secure"]["default"] = "1"
+
 	options = check_input(device_opt, process_input(device_opt))
 
 	## 
