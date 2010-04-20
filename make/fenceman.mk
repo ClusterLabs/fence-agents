3b302d1306320c2709d9feaebe37535d37fbddfe
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-04-20 14:59:42 +0200

    fence: fix make distcheck build out of tree
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/fenceman.mk
+++ make/fenceman.mk
@@ -1,6 +1,6 @@
 %.8: $(TARGET) $(top_srcdir)/fence/agents/lib/fence2man.xsl
 	set -e && \
-	PYTHONPATH=$(top_srcdir)/fence/agents/lib \
+	PYTHONPATH=$(top_srcdir)/fence/agents/lib:$(top_builddir)/fence/agents/lib \
 		python $^ -o metadata > .$@.tmp && \
 	xsltproc $(top_srcdir)/fence/agents/lib/fence2man.xsl .$@.tmp > $@
 
