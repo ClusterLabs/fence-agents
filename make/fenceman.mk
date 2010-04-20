319803209f2425ca02538d22e1750b2169e10d00
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-04-20 14:26:55 +0200

    fence agents automatic man page generation
    first import from STABLE3 branch
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ make/fenceman.mk
@@ -0,0 +1,8 @@
+%.8: $(TARGET) $(top_srcdir)/fence/agents/lib/fence2man.xsl
+	set -e && \
+	PYTHONPATH=$(top_srcdir)/fence/agents/lib \
+		python $^ -o metadata > .$@.tmp && \
+	xsltproc $(top_srcdir)/fence/agents/lib/fence2man.xsl .$@.tmp > $@
+
+clean-man:
+	rm -f *.8 .*.8.tmp
