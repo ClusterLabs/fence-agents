79336e5ef88fc33f40306466e48fafca9532d452
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-11-05 05:43:37 -0400

    overall cleanup and make ready for release
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/fenceman.mk
+++ make/fenceman.mk
@@ -1,6 +1,6 @@
 %.8: $(TARGET) $(top_srcdir)/fence/agents/lib/fence2man.xsl
 	set -e && \
-	PYTHONPATH=$(top_srcdir)/fence/agents/lib:$(top_builddir)/fence/agents/lib \
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib \
 		python $^ -o metadata > .$@.tmp && \
 	xsltproc $(top_srcdir)/fence/agents/lib/fence2man.xsl .$@.tmp > $@
 
