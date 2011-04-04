550a6ca4caee40bdf669e563e66371b23e564d6e
Marek 'marx' Grac <mgrac@redhat.com>
2011-04-04 15:19:57 +0200

    fence_ipmilan: Correct return code for diag operation
    Return code for diag operation should be 0.
    
    Replaces commit 8a97abd04b8492310c1095413c5cf26bbf98e395
    
    "diaf" != "diag"
    
    Resolves: rhbz#655764
    
    Signed-off-by: Lon Hohberger <lhh@redhat.com>

--- fence/agents/ipmilan/ipmilan.c
+++ fence/agents/ipmilan/ipmilan.c
@@ -1195,6 +1195,15 @@ metaout:
 	
 	if (!strcasecmp(op, "monitor"))
           translated_ret = ret;
+        
+        if (!strcasecmp(op, "diag")) {
+          /** .. but when a system receive the DIAG signal , it 
+           ** switches to a kdump kernel, but the machine is always
+           ** "on" during the dump phase. It only become temporarily
+           ** "off" at the end of the dump just before rebooting.
+           **/
+          translated_ret = 0;
+        }
 
 	return translated_ret;
 }
