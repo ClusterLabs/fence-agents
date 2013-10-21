7dd661fd3b4adf4f7ed4209598940983ca0ee243
Marek 'marx' Grac <mgrac@redhat.com>
2013-10-21 12:44:39 +0200

    testing: Check if fence agent uses only valid keys in options["--???"]
    Valid keys are defined in fencing library or fence agent itself. This check
    will be run at every build, so we should be protected against case when
    developer unintentionally use new key (e.g. typo). These type of bugs are
    usually not detected by static analyzers.

--- make/fencebuild.mk
+++ make/fencebuild.mk
@@ -1,4 +1,8 @@
 $(TARGET): $(SRC)
+	if [ 0 -eq `echo "$(SRC)" | grep fence_ &> /dev/null; echo $$?` ]; then \
+		PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib $(top_srcdir)/fence/agents/lib/check_used_options.py $(SRC); \
+	else true ; fi
+
 	bash $(top_srcdir)/scripts/fenceparse \
 		$(top_srcdir)/make/copyright.cf REDHAT_COPYRIGHT \
 		$(VERSION) \
