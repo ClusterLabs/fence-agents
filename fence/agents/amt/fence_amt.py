293325ff5be7a65a9a5300b40ebb7ee7907c832e
Ondrej Mular <omular@redhat.com>
2014-01-10 05:44:09 -0500

    fence_amt: add long description
    Signed-off-by: Marek 'marx' Grac <mgrac@redhat.com>

--- fence/agents/amt/fence_amt.py
+++ fence/agents/amt/fence_amt.py
@@ -134,7 +134,9 @@ def main():
 
     docs = { }
     docs["shortdesc"] = "Fence agent for AMT"
-    docs["longdesc"] = "Fence agent for AMT"
+    docs["longdesc"] = "fence_amt is an I/O Fencing agent \
+which can be used with Intel AMT. This agent calls support software amttool\
+(http://www.kraxel.org/cgit/amtterm/)."
     docs["vendorurl"] = "http://www.intel.com/"
     show_docs(options, docs)
 
