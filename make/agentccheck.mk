1d1838f4d8da8a59a9b7ad29030b3d69bc4ac07d
Marek 'marx' Grac <mgrac@redhat.com>
2014-05-21 15:35:53 +0200

    testing: fence_zvm agent is tested for working delay function
    also adds support for checking --delay for every C-based fence agent

--- make/agentccheck.mk
+++ make/agentccheck.mk
@@ -12,3 +12,11 @@ xml-check.%: %
 xml-upload.%: %
 	$(eval INPUT=$(subst xml-upload.,,$@))
 	./$(INPUT) -o metadata > $(DATADIR)/$(INPUT).xml
+
+# If test will fail, rerun fence agents to show problems
+delay-check.%: %
+	$(eval INPUT=$(subst delay-check.,,$@))
+	test `/usr/bin/time -f "%e" ./$(INPUT) --delay 10 $(FENCE_TEST_ARGS) -- 2>&1 |\
+	sed 's/\.//' | tail -n 1` -ge 1000 || \
+	/usr/bin/time -f "%e" ./$(INPUT) --delay 0 $(FENCE_TEST_ARGS) --
+
