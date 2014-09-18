076b9def6a956a3b634f8475f8ddece22fc8c271
Marek 'marx' Grac <mgrac@redhat.com>
2014-09-18 15:16:40 +0200

    [build] Create a new target for wiki export
    Create a list of arguments in wiki format

--- make/fenceman.mk
+++ make/fenceman.mk
@@ -4,6 +4,7 @@
 		python $(@:%.8=%) -o metadata > .$@.tmp && \
 	xmllint --noout --relaxng $(abs_srcdir)/../lib/metadata.rng .$@.tmp && \
 	xsltproc $(top_srcdir)/fence/agents/lib/fence2man.xsl .$@.tmp > $@
+	xsltproc $(top_srcdir)/fence/agents/lib/fence2wiki.xsl .$@.tmp | grep -v '<?xml' > $(@:%.8=%.wiki)
 
 clean-man:
-	rm -f *.8 .*.8.tmp
+	rm -f *.8 .*.8.tmp *.wiki
--- make/fencemanc.mk
+++ make/fencemanc.mk
@@ -3,6 +3,7 @@
 		./$^ -o metadata > .$@.tmp && \
 	xmllint --noout --relaxng $(top_srcdir)/fence/agents/lib/metadata.rng .$@.tmp && \
 	xsltproc $(top_srcdir)/fence/agents/lib/fence2man.xsl .$@.tmp > $@
+	xsltproc $(top_srcdir)/fence/agents/lib/fence2wiki.xsl .$@.tmp | grep -v '<?xml' > $(@:%.8=%.wiki)
 
 clean-man:
-	rm -f *.8 .*.8.tmp
+	rm -f *.8 .*.8.tmp *.wiki
