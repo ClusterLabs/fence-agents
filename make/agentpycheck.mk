8bd6050dccf93621066c5ae5cb8ae1953c331e63
Marek 'marx' Grac <mgrac@redhat.com>
2014-12-01 20:39:47 +0100

    fence_mpath: Do not run regression test on store_path value
    This is not possible because unlike the other paths this one
    is dependant on ./configure --prefix XXX. With this patch,
    line with value will be removed.

--- make/agentpycheck.mk
+++ make/agentpycheck.mk
@@ -1,18 +1,19 @@
 TEMPFILE:=$(shell mktemp)
 #DATADIR:=$(abs_top_builddir)/tests/data/metadata
 DATADIR:=$(abs_top_srcdir)/tests/data/metadata
+AWK='BEGIN {store=-1} /name=\"store_path\"/ {store=2} {if (store!=0) {print}; store--}'
 
 check: $(TARGET:%=xml-check.%) $(SYMTARGET:%=xml-check.%) $(TARGET:%=delay-check.%)
 
 xml-check.%: %
 	$(eval INPUT=$(subst xml-check.,,$@))
-	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata > $(TEMPFILE)
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | awk $(AWK) > $(TEMPFILE)
 	diff $(TEMPFILE) $(DATADIR)/$(INPUT).xml
 	rm $(TEMPFILE)
 
 xml-upload.%: %
 	$(eval INPUT=$(subst xml-upload.,,$@))
-	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata > $(DATADIR)/$(INPUT).xml
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | awk $(AWK) > $(DATADIR)/$(INPUT).xml
 
 # If test will fail, rerun fence agents to show problems
 delay-check.%: %
