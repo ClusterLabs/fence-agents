6b37cab7bc7fe41ce313d947b9c409c994c23451
Marek 'marx' Grac <mgrac@redhat.com>
2015-01-08 21:38:37 +0100

    [build] Replace using $AWK for different use then awk binary

--- make/agentpycheck.mk
+++ make/agentpycheck.mk
@@ -1,19 +1,19 @@
 TEMPFILE:=$(shell mktemp)
 #DATADIR:=$(abs_top_builddir)/tests/data/metadata
 DATADIR:=$(abs_top_srcdir)/tests/data/metadata
-AWK='BEGIN {store=-1} /name=\"store_path\"/ {store=2} {if (store!=0) {print}; store--}'
+AWK_VAL='BEGIN {store=-1} /name=\"store_path\"/ {store=2} {if (store!=0) {print}; store--}'
 
 check: $(TARGET:%=xml-check.%) $(SYMTARGET:%=xml-check.%) $(TARGET:%=delay-check.%)
 
 xml-check.%: %
 	$(eval INPUT=$(subst xml-check.,,$@))
-	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | awk $(AWK) > $(TEMPFILE)
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | $(AWK) $(AWK_VAL) > $(TEMPFILE)
 	diff $(TEMPFILE) $(DATADIR)/$(INPUT).xml
 	rm $(TEMPFILE)
 
 xml-upload.%: %
 	$(eval INPUT=$(subst xml-upload.,,$@))
-	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | awk $(AWK) > $(DATADIR)/$(INPUT).xml
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | $(AWK) $(AWK_VAL) > $(DATADIR)/$(INPUT).xml
 
 # If test will fail, rerun fence agents to show problems
 delay-check.%: %
