ClusterLabs/fence-agents

commit 364de00cdd7619ad65f5eabdab2d8499d448045d

fencing: Pexpect problems - missing return statement
Previusly pexpect.send() returned value which was not used anywhere, with new version this is no
longer true and our inherited class fspawn has to return proper values (length of the written string)

fence/agents/lib/fencing.py.py
@@ -393,11 +393,11 @@ class fspawn(pexpect.spawn):
 
 	def send(self, message):
 		logging.debug("Sent: %s" % message)
-		pexpect.spawn.send(self, message)
+		return pexpect.spawn.send(self, message)
 
 	# send EOL according to what was detected in login process (telnet)
 	def send_eol(self, message):
-		self.send(message + self.opt["eol"])
+		return self.send(message + self.opt["eol"])
 
 def atexit_handler():
 	try:

commit 41d30a804bb426707e87c4e57c194376e2828dcb

fencing: Password was awaited twice in some cases when connecting via telnet
Regression by commit 364de00cdd7619ad65f5eabdab2d8499d448045d

fence/agents/lib/fencing.py.py
@@ -1054,7 +1054,7 @@ def _login_telnet(options, re_login_string):
 		options["eol"] = "\n"
 		conn.send_eol(options["--username"])
 		conn.log_expect(re_pass, int(options["--login-timeout"]))
-	elif re_pass.search(screen) != None:
+	elif re_pass.search(screen) == None:
 		conn.log_expect(re_pass, int(options["--shell-timeout"]))
 
 	try: