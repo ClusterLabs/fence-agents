45dd8d3e297a5f8f35722aa057388f6abc236281
Andrew Beekhof <beekhof@Andrew-MBP16.local>
2016-07-06 17:23:41 +1000

    compute: Prevent use of undefined variable

--- fence/agents/compute/fence_compute.py
+++ fence/agents/compute/fence_compute.py
@@ -252,16 +252,20 @@ def fix_domain(options):
 
 	elif len(domains) == 1 and not options.has_key("--domain"):
 		options["--domain"] = last_domain
+		return options["--domain"]
 
 	elif len(domains) == 1:
 		logging.error("Overriding supplied domain '%s' does not match the one calculated from: %s"
 			      % (options["--domain"], hypervisor.hypervisor_hostname))
 		options["--domain"] = last_domain
+		return options["--domain"]
 
 	elif len(domains) > 1:
 		logging.error("The supplied domain '%s' did not match any used inside nova: %s"
 			      % (options["--domain"], repr(domains)))
 		sys.exit(1)
+
+	return None
 
 def fix_plug_name(options):
 	if options["--action"] == "list":
@@ -270,7 +274,7 @@ def fix_plug_name(options):
 	if not options.has_key("--plug"):
 		return
 
-	fix_domain(options)
+	calculated = fix_domain(options)
 	short_plug = options["--plug"].split('.')[0]
 	logging.debug("Checking target '%s' against calculated domain '%s'"% (options["--plug"], calculated))
 
