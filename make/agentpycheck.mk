075b6bb66cd6f100b1d6cbc937437d63d590b2ec
Oyvind Albrigtsen <oalbrigt@redhat.com>
2017-03-02 16:13:57 +0100

    CI: dont test paths in metadata

--- make/agentpycheck.mk
+++ make/agentpycheck.mk
@@ -1,18 +1,19 @@
 TEMPFILE:=$(shell mktemp)
-#DATADIR:=$(abs_top_builddir)/tests/data/metadata
 DATADIR:=$(abs_top_srcdir)/tests/data/metadata
+AWK_VAL='BEGIN {store=-1} /name=\".*_path\"/ {store=2} {if (store!=0) {print}; store--}'
 
 check: $(TARGET:%=xml-check.%) $(SYMTARGET:%=xml-check.%) $(TARGET:%=delay-check.%)
+xml-upload: $(TARGET:%=xml-upload.%) $(SYMTARGET:%=xml-upload.%)
 
 xml-check.%: %
 	$(eval INPUT=$(subst xml-check.,,$@))
-	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata > $(TEMPFILE)
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | $(AWK) $(AWK_VAL) > $(TEMPFILE)
 	diff $(TEMPFILE) $(DATADIR)/$(INPUT).xml
 	rm $(TEMPFILE)
 
 xml-upload.%: %
 	$(eval INPUT=$(subst xml-upload.,,$@))
-	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata > $(DATADIR)/$(INPUT).xml
+	PYTHONPATH=$(abs_srcdir)/../lib:$(abs_builddir)/../lib python ./$(INPUT) -o metadata | $(AWK) $(AWK_VAL) > $(DATADIR)/$(INPUT).xml
 
 # If test will fail, rerun fence agents to show problems
 delay-check.%: %
