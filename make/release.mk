3b597752006d6507f5f50a990904d5a5fb3248c4
Jan Pokorný <jpokorny@redhat.com>
2017-06-19 16:57:06 +0200

    build: release.mk: simplify the default goal, declare .PHONY targets
    The default goal line now serves mainly the documentation purpose,
    otherwise it could be squashed down to "sign" target.
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- make/release.mk
+++ make/release.mk
@@ -8,8 +8,12 @@ deliverables = $(project)-$(version).sha256 \
                $(project)-$(version).tar.gz \
                $(project)-$(version).tar.xz
 
-all: checks setup tag tarballs sha256 sign
 
+.PHONY: all
+all: tag tarballs sign  # first/last skipped per release/gpgsignkey respectively
+
+
+.PHONY: checks
 checks:
 ifeq (,$(version))
 	@echo ERROR: need to define version=
@@ -20,11 +24,15 @@ endif
 		exit 1; \
 	fi
 
+
+.PHONY: setup
 setup: checks
 	./autogen.sh
 	./configure
 	make maintainer-clean
 
+
+.PHONY: tag
 tag: setup ./tag-$(version)
 
 tag-$(version):
@@ -37,11 +45,15 @@ else
 	@touch $@
 endif
 
+
+.PHONY: tarballs
 tarballs: tag
 	./autogen.sh
 	./configure
 	make distcheck
 
+
+.PHONY: sha256
 sha256: $(project)-$(version).sha256
 
 # NOTE: dependency backtrack may fail trying to sign missing tarballs otherwise
@@ -52,6 +64,8 @@ $(project)-$(version).sha256:
 	# checksum anything from deliverables except for in-prep checksums file
 	sha256sum $(deliverables:$@=) | sort -k2 > $@
 
+
+.PHONY: sign
 ifeq (,$(gpgsignkey))
 sign: tarballs
 	@echo No GPG signing key defined
@@ -70,6 +84,8 @@ else
 		$<
 endif
 
+
+.PHONY: publish
 publish:
 ifeq (,$(release))
 	@echo Building test release $(version), no publishing!
@@ -81,5 +97,7 @@ else
 	@echo remember to update the wiki and send the email to cluster-devel and linux-cluster
 endif
 
+
+.PHONY: clean
 clean:
 	rm -rf $(project)* tag-* .tarball-version
