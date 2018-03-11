c36de74122af20db54362378e789c8feda3c5393
Hideo Yamauchi <renayama19661014@ybb.ne.jp>
2018-03-12 08:16:48 +0900

    Mid: fence_docker: Exclude slashes from the list.

--- fence/agents/docker/fence_docker.py
+++ fence/agents/docker/fence_docker.py
@@ -38,7 +38,7 @@ def get_list(conn, options):
 	output = send_cmd(options, "containers/json?all=1")
 	containers = {}
 	for container in output:
-		containers[container["Id"]] = (container["Names"][0], {True:"off", False: "on"}[container["Status"][:4].lower() == "exit"])
+		containers[container["Id"]] = ({True:container["Names"][0][1:], False: container["Names"][0]}[container["Names"][0][0:1] == '/'], {True:"off", False: "on"}[container["Status"][:4].lower() == "exit"])
 	return containers
 
 
